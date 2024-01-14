2de752ca225af73374aa0238c25d8010995517b2
Roozbeh Maheronnaghsh <jahesh.automation@gmail.com>
2024-01-14 16:52:42 +0330

    adding code to test openai embeding as an example

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from openai import OpenAI
+from dotenv import load_dotenv
 
 def main():
-    print("")
+    load_dotenv()
+    client = OpenAI()
+    response = client.embeddings.create(
+        input="Hello, I am Roozbeh and I work as a Developer in a company called Tadbir in Iran.",
+        model="text-embedding-ada-002"
+    )
+
+    print(response.data[0].embedding)
 
 if __name__ == "__main__":
     main()
